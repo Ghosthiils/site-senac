223ce59a3c2186e3715393dd3132f9edb1b10919
Luiz Fernando <luizfranca35mc@gmail.com>
2025-07-03 20:12:25 -0300

    Criando projeto
    Mudamos a logo dele, e colocamos novas frases,  dentro do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/icone.png'
+// import reactLogo from './assets/react.svg';
+import senaclogo from './assets/senac.webp'
 import './App.css'
 
 function App() {
@@ -9,24 +9,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+       
+        <a href="https://sp.senac.br/americana" target="_blank">
+          <img src={senaclogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Senac + React</h1>
+      <h1>Começando com react</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador é {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         Clique na imagem e confira nossos cursos
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       Beba água...
       </p>
     </>
   )
